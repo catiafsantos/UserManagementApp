dcf0c525f95364166871735ae1a8b9565281b64a
Cátia Ferreira <catia.ferreira@truphone.com>
2020-01-30 19:04:11 +0000

    Add project files.

---
+++ Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UserManagementApp.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string Role { get; set; }
+        public Boolean Active { get; set; }
+    }
+}
